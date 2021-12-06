cf5faac50a187e54e13825f192445c080a1e9acb
Rafaqs <queiros.rafael@gmail.com>
2021-12-06 16:06:40 -0300

    Add project files.

---
+++ SuperHeroiAPI/Data/HeroiContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroiAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroiAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> IdentidadeSecreta { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Password = sa123456; Persist Security Info = True; User ID = sa; Initial Catalog = HeroApp; Data Source = GAMES\\HERO");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+            });
+        }
+    }
+}
---
+++ SuperHeroiAPI/Migrations/20211206184151_Herois_Identidade.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperHeroiAPI.Migrations
+{
+    public partial class Herois_Identidade : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_identidadeSecretas_Herois_HeroiId",
+                table: "identidadeSecretas");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_identidadeSecretas",
+                table: "identidadeSecretas");
+
+            migrationBuilder.RenameTable(
+                name: "identidadeSecretas",
+                newName: "IdentidadeSecreta");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_identidadeSecretas_HeroiId",
+                table: "IdentidadeSecreta",
+                newName: "IX_IdentidadeSecreta_HeroiId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_IdentidadeSecreta",
+                table: "IdentidadeSecreta",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_IdentidadeSecreta_Herois_HeroiId",
+                table: "IdentidadeSecreta",
+                column: "HeroiId",
+                principalTable: "Herois",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_IdentidadeSecreta_Herois_HeroiId",
+                table: "IdentidadeSecreta");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_IdentidadeSecreta",
+                table: "IdentidadeSecreta");
+
+            migrationBuilder.RenameTable(
+                name: "IdentidadeSecreta",
+                newName: "identidadeSecretas");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_IdentidadeSecreta_HeroiId",
+                table: "identidadeSecretas",
+                newName: "IX_identidadeSecretas_HeroiId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_identidadeSecretas",
+                table: "identidadeSecretas",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_identidadeSecretas_Herois_HeroiId",
+                table: "identidadeSecretas",
+                column: "HeroiId",
+                principalTable: "Herois",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
