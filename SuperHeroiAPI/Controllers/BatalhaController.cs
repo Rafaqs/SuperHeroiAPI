9a41f9aebbdef4f5cdb7a5116110b7c35d0646ce
Rafaqs <queiros.rafael@gmail.com>
2021-12-08 15:27:04 -0300

    Controller Batalha e Heroi

---
+++ SuperHeroiAPI/Controllers/BatalhaController.cs
@@ -0,0 +1,89 @@
+﻿using Hero.Dominio;
+using Hero.Repo;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SuperHeroiAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BatalhaController : ControllerBase
+    {
+        private readonly HeroiContext _context;
+        public BatalhaController(HeroiContext context)
+        {
+           _context = context;
+        }
+
+        // GET: api/<BatalhaController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(new Batalha());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // GET api/<BatalhaController>/5
+        [HttpGet("{id}", Name = "GetBatalha")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BatalhaController>
+        [HttpPost]
+        public ActionResult Post(Batalha model)
+        {
+            try
+            {
+                _context.Add(model);
+                _context.SaveChanges();
+
+                return Ok("Add");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // PUT api/<BatalhaController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, Batalha model)
+        {
+            try
+            {
+                if(_context.Batalhas.AsNoTracking().FirstOrDefault(b => b.Id ==id) != null)
+                {
+                    _context.Update(model);
+                    _context.SaveChanges();
+
+                    return Ok("Alterado");
+                }
+                return Ok("Não Encontrado");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // DELETE api/<BatalhaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SuperHeroiAPI/Controllers/HeroiController.cs
@@ -0,0 +1,90 @@
+﻿using Hero.Dominio;
+using Hero.Repo;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SuperHeroiAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeroiController : ControllerBase
+    {
+        private readonly HeroiContext _context;
+
+        public HeroiController(HeroiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<HeroiController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(new Heroi());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // GET api/<HeroiController>/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            return Ok("value");
+        }
+
+        // POST api/Heroi
+        [HttpPost]
+        public ActionResult Post(Heroi model)
+        {
+            try
+            {
+                _context.Add(model);
+                _context.SaveChanges();
+                return Ok("Add");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // PUT api/<HeroiController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, Heroi model)
+        {
+            try
+            {
+                if (_context.Herois.AsNoTracking().FirstOrDefault(
+                    h => h.Id ==id) != null)
+                {
+                    _context.Update(model);
+                    _context.SaveChanges();
+
+                    return Ok("Alterado");
+                }
+                return Ok("Não Encontrado");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex}");
+            }
+        }
+
+        // DELETE api/<HeroiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
